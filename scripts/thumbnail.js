fb28d2444ee31906ff97028055155e4977f19750
lazoras <robert.guyett.85@gmail.com>
2016-04-17 13:27:38 -0400

    added target _blank for links. fixed header links so they do not show index.***. fixed thumbnail so that both images in the body and explicit photos get a routine to find the relative path.

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -6,23 +6,27 @@
 */
 hexo.extend.helper.register('thumbnail', function (post) {
     var url = post.thumbnail || '';
+    
+    //attempt to extract image from post
     if (!url) {
         var imgPattern = /\<img\s.*?\s?src\s*=\s*['|"]?([^\s'"]+).*?\>/ig;
         var result = imgPattern.exec(post.content);
         if (result && result.length > 1) {
-            url = result[1];
+            url = result[0];
         }
-        if(url.length > 0) {
-            var pattern = /^[\\{0,1}\/{0,1}]([^\/^\\]+)/,
-                pattern_ = /([^\/^\\]+)/;
-            if ((ret = pattern.exec(url)) != null) {
-                if(ret[0].length == url.length) {
-                    url = post.path + ret[1];
-                }
-            } else if ((ret = pattern_.exec(url)) != null) {
-                if(ret[0].length == url.length) {
-                    url = post.path + ret[1];
-                }
+    }
+
+    //check for relative path and inject post path if relative
+    if(url.length > 0) {
+        var pattern = /^[\\{0,1}\/{0,1}]([^\/^\\]+)/,
+            pattern_ = /([^\/^\\]+)/;
+        if ((ret = pattern.exec(url)) != null) {
+            if(ret[0].length == url.length) {
+                url = post.path + ret[1];
+            }
+        } else if ((ret = pattern_.exec(url)) != null) {
+            if(ret[0].length == url.length) {
+                url = post.path + ret[1];
             }
         }
     }
